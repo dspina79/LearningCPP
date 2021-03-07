422da13ac3d742c7d5eda8a16fcf91206bcfe8a0
Dave Spina <dspina79@gmail.com>
2021-03-06 21:09:53 -0500

    Added start of the basic concepts

---
+++ Basics/helloworld.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+// basic arithmetic and loops
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int multiply(int x, int y) {
+    return x * y;
+}
+
+void loopMessage(int times) {
+    for(int i = 0; i < times; i++) {
+        cout << "This is a new message at iteration " << (i + 1) << endl;
+    }
+}
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "The sum of 1 and 2 is " << add(1, 2) << endl;
+    cout << "The product of 2 and 4 is " << multiply(2, 4) << endl;
+    loopMessage(4);
+    return 0;
+}
