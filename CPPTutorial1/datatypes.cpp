62ff6a0389acb0bad8bb2495d81fb15c4f236757
Dave Spina <dspina79@gmail.com>
2021-03-09 20:53:46 -0500

    Custom functions and data types

---
+++ CPPTutorial1/datatypes.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void printASCII();
+
+int main() {
+    short x = 40;
+    unsigned short ux = 299;
+    int y = 1672747589;
+    unsigned int uy = 2672747589;
+    long l = 314981234974720;
+    long long ll = 7247923479299;
+    unsigned long long ull = 3789324589124791292;
+    
+    cout << "CHAR_MAX = " << CHAR_MAX << endl;
+    cout << "SHORT_MAX (Int16) = " << INT16_MAX << endl;;
+    cout << "INT_MAX = " << INT_MAX << endl;
+    cout << "ULONG_MAX = " << ULLONG_MAX << endl;
+    cout << "ULLONG_MAX = " << ULLONG_MAX << endl;
+    // 18,446,744,073,709,551,615 = 18 quntilion
+
+    bool bx = true;
+    if (bx) {
+        cout << "Well, of course x is true." << endl;
+    }
+
+    char weird = 55;
+    char weirdOther = 67;
+    cout << "CHAR(55) = " <<  weird << endl;
+    cout << "CHAR(67) = " <<  weirdOther << endl;
+    
+    printASCII();
+
+
+    return 0;
+}
+
+void printASCII() {
+    for (int i = 0; i < CHAR_MAX; i++) {
+        char c = i;
+        cout << "CHAR (" << i << "): " << c << endl;
+    } 
+}
--- CPPTutorial1/functions.cpp
+++ CPPTutorial1/functions.cpp
@@ -1,11 +1,18 @@
 #import <iostream>
 #import <cmath>
 
+using std::string;
 using std::cin;
 using std::cout;
 
+double add(double, double); // declaration
+void basicGreeter(string);
+
 int main() {
     int base, exponent;
+    double x, y;
+    string name;
+
     cout << "What is the base? ";
     cin >> base;
     cout << "What is the exponent? ";
@@ -13,5 +20,22 @@ int main() {
     double result = pow(base, exponent);
     cout << base << " to the power of " << exponent << " is " << result << "\n";
 
+    cout << "Enter in a number: ";
+    cin >> x;
+    cout << "Now another number: ";
+    cin >> y;
+    cout << "The sum of " << x << " and " << y << " is " << add(x, y) << std::endl;
+    cout << "Now enter your name: ";
+    cin >> name;
+    basicGreeter(name);
+
     return 0;
+}
+
+void basicGreeter(string name) {
+    cout << "Hello, " << name << std::endl;
+}
+
+double add(double x, double y) {
+    return x + y;
 }
