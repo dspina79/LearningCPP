8162e40dc71dd134bfeead1ca1d7a5481c56a1b8
Dave Spina <dspina79@gmail.com>
2021-03-13 20:03:42 -0500

    String getLine() example

--- CPPTutorial1/stringbasic.cpp
+++ CPPTutorial1/stringbasic.cpp
@@ -2,11 +2,26 @@
 #include <string>
 
 using std::cout;
+using std::cin;
 using std::endl;
 using std::string;
 
 string greet(string name)  {
     return "Hello, " + name;
+}
+
+void getInputWithoutGetLine() {
+    string motto;
+    cout << "Enter in your motto: ";
+    cin >> motto; // will only get the first word, the rest is left in buffer
+    cout << "The motto is " << motto << endl; 
+}
+
+void getInputWithGetLine() {
+    string motto;
+    cout << "What is your motto? ";
+    getline(cin, motto);
+    cout << "The motto is \"" << motto << "\"." endl;
 }
 
 int main() {
@@ -16,5 +31,8 @@ int main() {
     cout << greeting << " has a length of " << greeting.length() << endl;
     cout << greeting << " there" << endl;
     cout << greet("Dean") << endl;
+ 
+    getInputWithGetLine();
+ 
     return 0;
 }
